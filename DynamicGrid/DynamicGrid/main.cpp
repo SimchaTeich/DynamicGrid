5178775256a7a65c9f9ba29b97cb1c6119c3b276
SimchaTeich <sim.teich@gmail.com>
2022-10-20 01:44:52 +0300

    create the main program

--- DynamicGrid/DynamicGrid/main.cpp
+++ DynamicGrid/DynamicGrid/main.cpp
@@ -1,22 +1,69 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
 using std::cin;
-
+using std::string;
 
 void printGrid(const unsigned int height, const unsigned int width);
 
 
 int main()
 {
-	printGrid(10, 10);
+	string input;
+	unsigned int h;
+	unsigned int w;
+
+	while(true)
+	{
+		cout << "Welcome to the Print Grid System." << endl;
+		cout << "Enter height (as integer): ";
+		cin >> input;
+
+		// protect from malicious input.
+		try
+		{
+			h = std::stoi(input);
+		}
+		catch (...)
+		{
+			system("CLS");
+			continue;
+		}
+
+
+		cout << "Enter width (as integer): ";
+		cin >> input;
+
+		// protect from malicious input.
+		try
+		{
+			w = std::stoi(input);
+		}
+		catch (...)
+		{
+			system("CLS");
+			continue;
+		}
+
+		break;
+	}
+
+	// print the grid.
+	system("CLS");
+	printGrid(h, w);
 
 	system("PAUSE");
 	return 0;
 }
 
 
+/*
+* prints the grid
+* Input - height & width
+* Output - None.
+*/
 void printGrid(const unsigned int height, const unsigned int width)
 {
 	for (int i = 0; i < height; i++)
