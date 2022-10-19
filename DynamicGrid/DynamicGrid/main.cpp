5767ad3f01ee6da75fb5baf8559e1222607673e8
SimchaTeich <sim.teich@gmail.com>
2022-10-20 02:48:23 +0300

    build function for safe input from user.

--- DynamicGrid/DynamicGrid/main.cpp
+++ DynamicGrid/DynamicGrid/main.cpp
@@ -7,55 +7,55 @@ using std::cin;
 using std::string;
 
 void printGrid(const unsigned int height, const unsigned int width);
+unsigned int getValue(string nameOfValue);
 
 
 int main()
 {
 	string input;
-	unsigned int h;
-	unsigned int w;
+	unsigned int h = getValue("height");
+	unsigned int w = getValue("width");
 
-	while(true)
-	{
-		cout << "Welcome to the Print Grid System." << endl;
-		cout << "Enter height (as integer): ";
-		cin >> input;
-
-		// protect from malicious input.
-		try
-		{
-			h = std::stoi(input);
-		}
-		catch (...)
-		{
-			system("CLS");
-			continue;
-		}
-
-
-		cout << "Enter width (as integer): ";
-		cin >> input;
-
-		// protect from malicious input.
-		try
-		{
-			w = std::stoi(input);
-		}
-		catch (...)
-		{
-			system("CLS");
-			continue;
-		}
-
-		break;
-	}
-
+	
 	// print the grid.
 	system("CLS");
 	printGrid(h, w);
 
 	system("PAUSE");
 	return 0;
+}
+
+
+/*
+* function returns an integer value from user.
+* if input is worng, try again.
+* Input - name of the value (for represent it)
+* Output - the valid value from user type int.
+*/
+unsigned int getValue(string nameOfValue)
+{
+	string input;
+	unsigned int value;
+
+	while (true)
+	{
+		cout << "Enter "<< nameOfValue << " (as integer) : ";
+		cin >> input;
+
+		// protect from malicious input.
+		try
+		{
+			value = std::stoi(input);
+		}
+		catch (...)
+		{
+			continue;
+		}
+
+		break;
+	}
+
+	return value;
 }
 
 
